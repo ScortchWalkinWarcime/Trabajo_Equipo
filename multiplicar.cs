68fad9bae8933d474c717417a76d9086d682295a
Olavi04 <160789479+Olavi04@users.noreply.github.com>
2025-05-19 10:07:27 -0600

    Update multiplicar.cs

--- multiplicar.cs
+++ multiplicar.cs
@@ -1,1 +1,17 @@
+using System;
 
+class Program
+{
+    static void Main()
+    {
+        // Declarar los números
+        int numero1 = 5;
+        int numero2 = 10;
+
+        // Multiplicar
+        int resultado = numero1 * numero2;
+
+        // Mostrar el resultado
+        Console.WriteLine("El resultado de la multiplicación es: " + resultado);
+    }
+}
